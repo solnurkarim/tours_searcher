a1501a2af895dc96f6fad619c977520df174a092
solspyre <solnurkarim@gmail.com>
2018-02-09 12:08:25 +0600

    testing build ready

--- script.js
+++ script.js
@@ -28,28 +28,36 @@ $(document).ready(function(){
 	let mealsIdList = new Object();
 
 	let tourDatesList;
-	let tourDatesTagSelected;
-	let tourDatesTagSelectedId;
-	let tourDatesIdList = new Object();
 
-
+	// загрузка списка стран
 	$.get(source + 'GetDepartCities?', function(data){
 		cityList = data.GetDepartCitiesResult.Data;
 		for(i=0; i<cityList.length; i++){
 			let city = cityList[i].Name;
 			cityIdList[city] = cityList[i].Id;
-			$('#city').append('<option>'+ city +'</option');
+			$('#city').append('<option>'+ city +'</option>');
+		}
+	});
+
+	// загрузка списка типов питания
+	$.get(source + 'GetMeals?', function(data){
+		mealsList = data.GetMealsResult.Data;
+		for(i=0; i<mealsList.length; i++){
+			let meals = mealsList[i].Name;
+			mealsIdList[meals] = mealsList[i].Id;
+			$('#meals').append('<option>'+ meals +'</option>');
 		}
 	});
 
 
-
+	// обработчик выбора города вылета
 	$('#city').change(function(){
 		cityTagSelected = this.value;
 		cityTagSelectedId = cityIdList[cityTagSelected];
 		loadCountryTagList();
 	});
 
+	// загрузка списка стран по выбранному городу вылета
 	function loadCountryTagList() {
 		$('#country').html('<option>Направление</option');
 		$.get(source + 'GetCountries?townFromId=' + cityTagSelectedId, function(data){
@@ -57,18 +65,19 @@ $(document).ready(function(){
 			for (i=0; i<countryList.length; i++) {
 			let country = countryList[i].Name;
 			countryIdList[country] = countryList[i].Id;
-			$('#country').append('<option>'+ country +'</option');
+			$('#country').append('<option>'+ country +'</option>');
 			}
 		});
 	}
 
-
+	// обработчик выбора направления
 	$('#country').change(function(){
 		countryTagSelected = this.value;
 		countryTagSelectedId = countryIdList[countryTagSelected];
 		loadResortTagList();
 	});
 
+	// загрузка списка курортов по выбранному направлению
 	function loadResortTagList() {
 		$('#resort').html('<option>Курорт</option');
 		$.get(source + 'GetCities?countryId=' + countryTagSelectedId, function(data){
@@ -76,14 +85,26 @@ $(document).ready(function(){
 			for(i=0; i<resortList.length; i++){
 				let resort = resortList[i].Name;
 				resortIdList[resort] = resortList[i].Id;
-				$('#resort').append('<option>'+ resort +'</option');
+				$('#resort').append('<option>'+ resort +'</option>');
 			}
 		});
 	}
 
+	// обработчик выбора курорта
+	$('#resort').change(function(){
+		resortTagSelected = this.value;
+		resortTagSelectedId = resortIdList[resortTagSelected];
+		if(resortTagSelectedId == undefined) {
+			resortTagSelectedId = '';
+		}
+		loadHotelTagList();
+		loadTourDatesTagList();
+	});
+
+	// обработчик выбранных категорий отелей.
 	// Заменить на добавление кодов категорий в массив с последующим 
 	// объединением элементов массива в строку через разделитель.
-	// Добавить остальные категории отелей
+	// Добавить остальные категории отелей.
 	$('input:checkbox').change(function(){
 		
 			starsQueries = '';
@@ -114,15 +135,8 @@ $(document).ready(function(){
 			loadHotelTagList();
 	});
 
-	$('#resort').change(function(){
-		resortTagSelected = this.value;
-		resortTagSelectedId = resortIdList[resortTagSelected];
-		if(resortTagSelectedId == undefined) {
-			resortTagSelectedId = '';
-		}
-		loadHotelTagList();
-	});
-
+	
+	// загрузка списка отелей по выбранному курорту
 	function loadHotelTagList() {
 		$('#hotel').html('<option>Отели</option');
 		$.get(source + 'GetHotels?countryId=' + countryTagSelectedId + "&towns=" + resortTagSelectedId + starsQueries + '&all=-1', function(data){
@@ -130,39 +144,23 @@ $(document).ready(function(){
 			for(i=0; i<hotelList.length; i++){
 				let hotel = hotelList[i].Name;
 				hotelIdList[hotel] = hotelList[i].Id;
-				$('#hotel').append('<option>'+ hotel +'</option');
+				$('#hotel').append('<option>'+ hotel +'</option>');
 			}
 		});
 	}
 
-
-	$('#meals').html('<option>Типы питания</option');
-	$.get(source + 'GetMeals?', function(data){
-		mealsList = data.GetMealsResult.Data;
-		for(i=0; i<mealsList.length; i++){
-			let meals = mealsList[i].Name;
-			hotelIdList[meals] = mealsList[i].Id;
-			$('#meals').append('<option>'+ meals +'</option');
-		}
-	});
-
-	$('#hotel').change(function(){
-		hotelTagSelected = this.value;
-		hotelTagSelectedId = hotelIdList[hotelTagSelected];
-		loadTourDatesTagList();
-	});
-
+	// загрузка дат вылета по выбранному курорту
 	function loadTourDatesTagList() {
 		$('#tour-dates').html('<option>Доступные даты тура</option');
 		$.get(source + 'GetTourDates?dptCityId=' + cityTagSelectedId + "&countryId=" + countryTagSelectedId + '&resorts=' + resortTagSelectedId, function(data){
-			tourDatesList = data.GetTourDatesResult.Data;
+			tourDatesList = data.GetTourDatesResult.Data.dates;
 			for(i=0; i<tourDatesList.length; i++){
-				let tourDates = tourDatesList[i].Name;
-				tourDatesIdList[tourDates] = tourDatesList[i].Id;
-				$('#tour-dates').append('<option>'+ tourDates +'</option');
+				let tourDates = tourDatesList[i];
+				$('#tour-dates').append('<option>'+ tourDates +'</option>');
 			}
 		});
 	}
 
+	
 
 });
